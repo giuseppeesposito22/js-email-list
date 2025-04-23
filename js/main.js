d1f6c62e95e5726339534b40fec567aba187fa6d
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-23 15:56:25 +0200

    generated email list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("ok");
+const ulEl = document.getElementById("email-list");
+
+const emailArray = [];
+const emailApi = `https://flynn.boolean.careers/exercises/api/random/mail`;
+
+for (let i = 0; i < 10; i++) {
+  axios.get(emailApi).then((response) => {
+    const email = response.data.response;
+
+    emailArray.push(email);
+
+    if (emailArray.length === 10) {
+      for (const email of emailArray) {
+        ulEl.innerHTML += `<li class="list-group-item">${email}</li>`;
+      }
+    }
+  });
+}
